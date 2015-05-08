a4b38a9a5f69c138937c1230730ac4e5c36b35a3
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-05-07 18:09:52 -0700

    file finder is better for searching

--- src/Components/ShowDocs.jsx
+++ src/Components/ShowDocs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AllDocsStore from '../stores/AllDocs';
 import {getAttachmentUrl} from "../utils/database";
+import {Table, Alert, Input} from "react-bootstrap"
 
 import _ from "underscore";
 
@@ -26,7 +27,7 @@ let Attachment = React.createClass({
 
 export default React.createClass({
   getInitialState() {
-    return {docs: AllDocsStore.getState().docs};
+    return {docs: AllDocsStore.getState().docs, searchterm:""};
   },
   componentDidMount() {
     AllDocsStore.listen(this.onChange)
@@ -37,7 +38,35 @@ export default React.createClass({
   },
 
   onChange() {
-    this.setState(this.getInitialState());
+    this.setState({docs: AllDocsStore.getState().docs});
+  },
+
+  changeSearch(evt){
+    evt.defaultPrevented = true;
+    this.setState({searchterm: evt.target.value})
+  },
+
+  cancelSearch(){
+    this.setState({searchterm: null});
+  },
+
+  onSearchKeyUp(evt){
+    // reset on ESC:
+    evt.keyCode === 27 && this.cancelSearch();
+  },
+
+  _getAttachments(){
+    var files = _.flatten(_.map(_.pairs(this.state.docs), function([id, doc]){
+      return _.map(_.pairs(doc._attachments), function([name, a]){
+        return {doc: doc, name: name, attachment: a};
+      })
+    }));
+    if (this.state.searchterm) {
+      var term = this.state.searchterm.toLowerCase();
+      return _.filter(files, x => x.name.toLowerCase().indexOf(term) > -1)
+    }
+
+    return files;
   },
 
   render(){
@@ -45,16 +74,31 @@ export default React.createClass({
       return <p>No Docs found</p>
     }
 
-    var attachments = _.flatten(_.map(_.pairs(this.state.docs), function([id, doc]){
-      return _.map(_.pairs(doc._attachments), function([name, a]){
-        return (<Attachment doc={doc} name={name} attachment={a} />)
-      })
-    }));
-    console.log(attachments);
+    var attachments = _.map(this._getAttachments(),
+                            props => <tr><td><Attachment {...props} /></td></tr> );
+
+    if (!attachments.length){
+      attachments = (<tr><td><Alert bsStyle='warning'>
+        <p>No document matching the criteria found.</p>
+        </Alert></td></tr>)
+    }
+
     return (
-      <ul>
-        {attachments}
-      </ul>
+      <Table striped bordered condensed hover>
+        <thead>
+          <tr>
+            <Input type="search"
+              addonBefore="Type to search:"
+              value={this.state.searchterm}
+              onChange={this.changeSearch}
+              onKeyUp={this.onSearchKeyUp}
+              placeholder="filename" />
+          </tr>
+        </thead>
+        <tbody>
+          {attachments}
+        </tbody>
+      </Table>
       )
   }
 });
